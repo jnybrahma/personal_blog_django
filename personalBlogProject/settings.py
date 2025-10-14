1c44c90199f5ecba4d15a75e0b4b169cbc7e86bc
Jnyananjoy Brahma <jnybrahma@gmail.com>
2025-10-14 20:13:48 +1000

    settings

--- personalBlogProject/settings.py
+++ personalBlogProject/settings.py
@@ -54,6 +54,29 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 ROOT_URLCONF = 'personalBlogProject.urls'
 
 TEMPLATES = [
